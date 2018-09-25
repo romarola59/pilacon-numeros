9aa60292d22f32291426a8a167239e03d25beda4
Rosa M. Rodriguez <romarola@hotmail.com>
2018-09-25 20:45:25 -0300

    pila numeros invertidos

--- pila connumerosingresados por el usuario/Program.cs
+++ pila connumerosingresados por el usuario/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.Collections;
 
 namespace pila_connumerosingresados_por_el_usuario
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            int numero = 0, numeros = 0;
+            Stack miPila = new Stack();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Ingrese un numero Entero:");
+                numero = Convert.ToInt32(Console.ReadLine());
+                miPila.Push(numero);
+                
+
+            }
+               Console.WriteLine("Los numeros ingresados son:");
+            for (int i = 0; i < 5; i++)
+            {
+                numeros = (int)miPila.Pop();
+                Console.WriteLine(numeros);
+                
+
+            }
+            Console.ReadKey();
         }
     }
 }
